3df2050f58790be49cfd03f8e9415abac16f7e86
Alok Prabhakare <alok.prabhkar@infinitysoftsystems.com>
2025-05-21 10:19:10 +0530

    updated awsConfig

---
+++ src/awsConfig.js
@@ -0,0 +1,9 @@
+import { CognitoUserPool } from 'amazon-cognito-identity-js';
+
+export const poolData = {
+  UserPoolId: process.env.REACT_APP_COGNITO_USER_POOL_ID,
+  ClientId: process.env.REACT_APP_COGNITO_CLIENT_ID,
+};
+
+const userPool = new CognitoUserPool(poolData);
+export default userPool;
